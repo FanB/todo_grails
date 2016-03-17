e6a7935b5a09b6ea5ab0c518e17b3053aee66e5d
Francois Bauduin <francois.bauduin@gmail.com>
2016-03-17 20:58:35 +0100

    deport de certaines valeurs dans le fichier de config + utilisation d'un moteur de template pour la connexion au ldap

--- grails-app/controllers/demangular/SessionController.groovy
+++ grails-app/controllers/demangular/SessionController.groovy
@@ -8,11 +8,22 @@ class SessionController {
 	def index() { }
 
     def create() { 
+    	//recuperation des parametres de la requete (login & password)
     	String login = params.login
     	String password = params.password
         try{
-    	   LDAPConnection connection = new LDAPConnection('10.125.0.64',1390)
-    	   BindResult bindResult = connection.bind("uid=${login},ou=people,dc=sli,dc=dom",password)
+           //recuperation de l'adresse du serveur ldap et de son port via le fichier application.yml
+           String host = grailsApplication.config.getProperty('config.ldap.host',String)
+           Integer port = grailsApplication.config.getProperty('config.ldap.port',Integer)
+           //connexion au ldap avec unboundid
+    	   LDAPConnection connection = new LDAPConnection(host,port)
+    	   //recuperation du string de filtre pour le bind dans application.yml
+    	   String bindFilter = grailsApplication.config.getProperty('config.ldap.bind-filter',String)
+    	   //utilisation d'un moteur de template afin de reconstruire le filtre de binding avec les parametres de la requete
+    	   def binding = ["login":login]
+    	   def engine = new groovy.text.SimpleTemplateEngine()
+    	   def template = engine.createTemplate(bindFilter).make(binding)
+    	   BindResult bindResult = connection.bind(template.toString(),password)
     	   if (bindResult.resultCode == ResultCode.SUCCESS){
     		  session["login"] = login
     		  render(view:'/index')
@@ -22,6 +33,7 @@ class SessionController {
     		  redirect(controller: "session", action: "new")
     	   }
         }catch(Exception e){
+        	log.error e.message
             flash.message = e.message
             redirect(controller: "session", action: "new")
         }
